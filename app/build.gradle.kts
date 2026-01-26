de18216e0e1aa14b325d443b6f1b53a3608c8f17
elvyn <elvyn.paucar.ponce@gmail.com>
2026-01-26 14:42:01 -0500

    add coil for image loading

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.coil.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.retrofit.main)
     implementation(libs.retrofit.gson)
