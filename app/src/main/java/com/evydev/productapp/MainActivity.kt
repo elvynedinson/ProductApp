a2c857fcf09af80146526e03ab359bbfbc9bfb71
elvyn <elvyn.paucar.ponce@gmail.com>
2026-01-26 14:36:50 -0500

    fix: add INTERNET permission to AndroidManifest

--- app/src/main/java/com/evydev/productapp/MainActivity.kt
+++ app/src/main/java/com/evydev/productapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.evydev.productapp.ui.screen.ProductScreen
 import com.evydev.productapp.ui.theme.ProductAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ProductAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            ProductScreen()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProductAppTheme {
-        Greeting("Android")
     }
 }
