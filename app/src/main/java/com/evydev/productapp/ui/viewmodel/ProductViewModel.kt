96e20738ab55664c6cebe63910a9f4a6e7a642be
elvyn <elvyn.paucar.ponce@gmail.com>
2026-01-26 14:14:28 -0500

    add product viewmodel with stateflow

---
+++ app/src/main/java/com/evydev/productapp/ui/viewmodel/ProductViewModel.kt
@@ -0,0 +1,35 @@
+package com.evydev.productapp.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.evydev.productapp.data.remote.RetrofitClient.api
+import com.evydev.productapp.ui.state.ProductUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class ProductViewModel: ViewModel(){
+
+    private val _state = MutableStateFlow<ProductUiState>(ProductUiState.Loading)
+    val state: StateFlow<ProductUiState> = _state
+
+    init {
+        loadingProducts()
+    }
+
+    private fun loadingProducts() {
+
+        viewModelScope.launch {
+            _state.value = ProductUiState.Loading
+
+            try {
+                val products = api.getProducts()
+                _state.value = ProductUiState.Success(products)
+
+            }catch (e: Exception){
+                _state.value = ProductUiState.Error("Error al cargar los Productos")
+            }
+        }
+    }
+}
+
