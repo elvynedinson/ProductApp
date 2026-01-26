ee1224217ffae0694d7da48ca476f57d922ccb2b
elvyn <elvyn.paucar.ponce@gmail.com>
2026-01-26 15:06:30 -0500

    ui: center loading indicator

--- app/src/main/java/com/evydev/productapp/ui/screen/ProductScreen.kt
+++ app/src/main/java/com/evydev/productapp/ui/screen/ProductScreen.kt
@@ -1,11 +1,16 @@
 package com.evydev.productapp.ui.screen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.evydev.productapp.data.model.Product
@@ -21,7 +26,12 @@ fun ProductScreen(
 
     when(state){
         is ProductUiState.Loading ->{
-            Text("Cargando Productos...")
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
         }
 
         is ProductUiState.Success -> {
