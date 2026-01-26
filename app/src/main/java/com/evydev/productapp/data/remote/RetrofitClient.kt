767ddab1fe6a69299b300f257fe7f6ecd9eacf01
elvyn <elvyn.paucar.ponce@gmail.com>
2026-01-26 13:54:52 -0500

    configure retrofit client

---
+++ app/src/main/java/com/evydev/productapp/data/remote/RetrofitClient.kt
@@ -0,0 +1,17 @@
+package com.evydev.productapp.data.remote
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient{
+
+    private const val BASE_URL = "https://fakestoreapi.com/"
+
+    val api: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
