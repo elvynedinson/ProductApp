36453c4c2f7efe5edcf707a90eacc0ba116be254
elvyn <elvyn.paucar.ponce@gmail.com>
2026-01-27 22:23:40 -0500

    di: inject ProductRepository with Hilt

--- app/src/main/java/com/evydev/productapp/data/repository/ProductRepository.kt
+++ app/src/main/java/com/evydev/productapp/data/repository/ProductRepository.kt
@@ -2,8 +2,9 @@ package com.evydev.productapp.data.repository
 
 import com.evydev.productapp.data.model.Product
 import com.evydev.productapp.data.remote.ApiService
+import javax.inject.Inject
 
-class ProductRepository(
+class ProductRepository @Inject constructor(
     private val api: ApiService
 ){
     suspend fun getProducts(): List<Product> {
